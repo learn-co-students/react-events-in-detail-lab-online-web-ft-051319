6fbae90f5d32c414c0a4fc5c767aff67bcef0f2d
mkonig411blk <margot.konig@blackrock.com>
2019-09-25 22:36:23 -0500

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,17 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react';
+
+export default class DelayedButton extends Component {
+
+// Create a button that, when clicked, will pass the click event to the onDelayedClick prop within a setTimeout(). The setTimeout() should be set to this.props.delay.
+    onClick = event => {
+        event.persist()
+        setTimeout(() => {
+            this.props.onDelayedClick(event) }, this.props.delay)
+    }
+
+    render() {
+        return <button onClick={this.onClick}>Delayed</button>
+    }
+
+}
